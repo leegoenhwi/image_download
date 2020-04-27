f734a60f8dd21df691f009f8a2c379e009deb4c3
leegoenhwi <10131751z@gmail.com>
2020-04-28 00:22:06 +0900

    json, js with image

--- app/src/main/java/com/personal_project/image_download/Download.java
+++ app/src/main/java/com/personal_project/image_download/Download.java
@@ -1,12 +1,14 @@
 package com.personal_project.image_download;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -30,12 +32,14 @@ import java.util.ArrayList;
 public class Download extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView dowmload_back_arrow;
-    private String htmlpageURL = "https://gothiczzang.tistory.com/16";
+    private String htmlpageURL = "";
     private ArrayList<String> image_list;
     private Thread thread;
     private Crawling crawling;
     private Crawling_Handler handler;
     private TextView one;
+
+    private WebView webView;
 
     @SuppressLint("HandlerLeak")
     @Override
@@ -43,11 +47,32 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.download);
 
-
         initui();
 
+        thread.start();
+        //지정한 URL을 웹 뷰로 접근하기
+        webView.loadUrl(htmlpageURL);
 
 
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void js_setting()
+    {
+        // 자바스크립트인터페이스 연결
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 이걸 통해 자바스크립트 내에서 자바함수에 접근할 수 있음.
+        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+        // 페이지가 모두 로드되었을 때, 작업 정의
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
+                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
+                view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
+            }
+        });
     }
 
     private void getURL()
@@ -64,6 +89,7 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
 
 
         image_list = new ArrayList<String>();
+        webView = new WebView(this);
         handler = new Crawling_Handler();
         crawling = new Crawling();
     }
@@ -80,6 +106,7 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
         dowmload_back_arrow.setOnClickListener(this);
         thread_setting();
         getURL();
+        js_setting();
     }
 
     @Override
@@ -95,10 +122,40 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onResume() {
 
-        thread.start();
+//        thread.start();
         super.onResume();
 
     }
+
+
+    public class MyJavascriptInterface {
+        @JavascriptInterface
+        public void getHtml(String html) {
+            //위 자바스크립트가 호출되면 여기로 html이 반환됨
+
+            System.out.println("실행 중");
+
+//            System.out.println("source");
+//            System.out.println(source);
+
+            try {
+                Document doc = Jsoup.parse(html);
+
+                for (Element e : doc.select("img")) {
+
+                    if (e.attr("src") != null) {
+                        image_list.add(e.attr("src"));
+                    }
+
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            handler.sendEmptyMessage(0);
+        }
+    }
+
 
     class Crawling implements Runnable{
 
@@ -127,7 +184,7 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
                 e.printStackTrace();
             }
 
-            handler.sendEmptyMessage(0);
+            //handler.sendEmptyMessage(0);
         }
 
 
@@ -144,13 +201,15 @@ public class Download extends AppCompatActivity implements View.OnClickListener{
 
                     for(int i =0;i< image_list.size();i++) {
                         empty += image_list.get(i);
+                        System.out.println(image_list.get(i));
                     }
 
                     one.setText(empty);
 
                 }
+
             }
-        }
+    }
 
 
 }
